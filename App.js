f783b20427b79b0d7ff472d72c51c6c1c33319a8
mikegomdi <mikegomez1493@gmail.com>
2021-03-25 16:51:06 -0600

    4 commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {{fontSize:30}}>Hello World</Text>
+      <TouchableOpacity onPress = {()=> Alert.alert('Esto es touch')}>
+        <Text style = {{fontSize:20}}>click me!</Text>
+      </TouchableOpacity>
+      <Button title='Aceptar' onPress = {()=> Alert.alert('Esto es touch x2 :V')}></Button>
     </View>
   );
 }
